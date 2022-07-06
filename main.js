591a8fc3af76f1a7b1eeeb815a57f6ea243cbaf5
TemiCodes <86966766+TemiCodes@users.noreply.github.com>
2022-07-06 10:25:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+status=""
+function setup() {
+    canvas=createCanvas(350,350)
+    canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
+}
+function draw() {
+    image(video,0,0,350,350)
+}
+function Start() {
+    objectdetector=ml5.objectDetector("cocossd",modeloaded)
+    document.getElementById("Status").innerHTML = "Status: Identifying Objects"
+}
+function modeloaded() {
+    console.log("modelisLoaded")
+    status=true
+}
